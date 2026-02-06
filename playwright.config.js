e321bac96b2b30915960e68de4c477a8a44b4093
testdino-inc <support@testdino.com>
2026-02-06 10:07:24 +0530

    live streaming test

--- playwright.config.js
+++ playwright.config.js
@@ -36,7 +36,7 @@ export default defineConfig({
 
   projects: [
     {
-      name: 'chromium',
+      name: 'kriti',
       use: { ...devices['Desktop Chrome'] },
       grep: /@chromium/, // only run tests tagged @chromium
     },
