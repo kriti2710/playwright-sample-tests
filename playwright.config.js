4e2d742883259c3258903e0c53a568b8697eafba
Kriti Verma <155474803+kriti2710@users.noreply.github.com>
2026-03-04 21:20:27 +0530

    Replace hardcoded token with environment variable

--- playwright.config.js
+++ playwright.config.js
@@ -23,10 +23,8 @@ export default defineConfig({
     ['json', { outputFile: './playwright-report/report.json' }],
 
     ['@testdino/playwright', {
-      token: "trx_staging_98d2409fbe2209d66cc8c11d1f032a155c359aad4040f63af4f5f90ea00c21c8",
+      token: process.env.TESTDINO_TOKEN,
       serverUrl: 'https://staging-api.testdino.com',
-      // token: "trx_development_ceb65e2394faabbd238d1be3a7637334dc8414799cde63e7d68c7a1860f814bf",
-      // serverUrl: 'http://localhost:3001',
     }],
   ],
 
