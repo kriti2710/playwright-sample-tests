c37df0f04a37c4d172f805692a0db6052de360c3
dhruvi-alphabin <dhruvi.balar.alphabin@gmail.com>
2026-07-10 11:23:27 +0530

    Update config and yml to fix live streaming on GH

--- playwright.config.js
+++ playwright.config.js
@@ -1,61 +1,76 @@
 // @ts-check
-import { defineConfig, devices } from '@playwright/test';
-import * as dotenv from 'dotenv';
 
-dotenv.config({ quiet: true });
+import { defineConfig, devices } from '@playwright/test';
+import dotenv from 'dotenv';
+
+// Load environment variables
+dotenv.config();
+
 const isCI = !!process.env.CI;
 
 export default defineConfig({
   testDir: './tests',
+  snapshotDir: './__screenshots__',  // :white_check_mark: Baseline image storage
   fullyParallel: true,
-  forbidOnly: isCI,
-  retries: isCI ? 1 : 0,
-  workers: isCI ? 1 : 1,
-  
+  // forbidOnly: isCI,
+  retries: isCI ? 1 : 1, // Enable retries for flaky test behavior
+  workers: isCI ? 3 : 3,
 
-  timeout: 30 * 1000,
+  timeout: 60 * 1000,
+  expect: {
+    timeout: 10 * 1000,
+  },
+  
   reporter: [
-    ['html', {
-      outputFolder: 'playwright-report',
-      open: 'never'
-    }],
-    ['blob', { outputDir: 'blob-report' }], // Blob reporter for merging
-    ['json', { outputFile: './playwright-report/report.json' }],
+  [
+    '@testdino/playwright',
+    {
+      serverUrl: 'https://stg-analytics.testdino.com',
+      token: 'td_api_8bc45cf4a5982e20a592a028fa08f538bc21d48020858f9157b14e4fe8104282',
+      debug: true,
+      artifacts: true,
+    },
   ],
+  ['html', { outputFolder: './playwright-report', open: 'never' }],
+  ['json', { outputFile: 'report.json' }],
+],
 
   use: {
-    baseURL: 'storedemo.testdino.com',
+    baseURL: 'https://storedemo.testdino.com/products',
     headless: true,
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
+    actionTimeout: 15 * 1000,
+    navigationTimeout: 30 * 1000,
   },
 
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-      grep: /@chromium/, // only run tests tagged @chromium
     },
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
-      grep: /@firefox/, // only run tests tagged @firefox
     },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-      grep: /@webkit/, // only run tests tagged @webkit
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    //   grep: /@webkit/, // only run tests tagged @webkit
+    // },
     {
       name: 'android',
       use: { ...devices['Pixel 5'] },
-      grep: /@android/, // only run tests tagged @android
     },
     {
       name: 'ios',
       use: { ...devices['iPhone 12'] },
-      grep: /@ios/, // only run tests tagged @ios
+    },
+
+    {
+      name: 'api',
+      use: { ...devices['API'] },
     },
   ],
 });
